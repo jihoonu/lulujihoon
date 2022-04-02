9932a4dc6f152f92f4e0aec88bbccded7649faa3
jihoo <jihoonu13@gmail.com>
2022-04-02 13:27:44 +0900

    first commit

---
+++ src/test/java/com/LULUJIHOON/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LULUJIHOON.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
